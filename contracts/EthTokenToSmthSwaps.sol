333c88b4d9a9e5551633147944a020bc90b4889a
Pavel Ivanov <grammka@gmail.com>
2018-06-07 19:50:56 +0300

    audit 4.1.2 fix

--- contracts/EthTokenToSmthSwaps.sol
+++ contracts/EthTokenToSmthSwaps.sol
@@ -91,7 +91,7 @@ contract EthTokenToSmthSwaps {
   // ETH Owner closes swap
   // ETH Owner receive +1 reputation
   function close(address _participantAddress) public {
-    require(swaps[msg.sender][_participantAddress].secret != bytes32(0));
+    require(swaps[msg.sender][_participantAddress].balance == 0);
 
     Reputation(ratingContractAddress).change(msg.sender, 1);
     clean(msg.sender, _participantAddress);
