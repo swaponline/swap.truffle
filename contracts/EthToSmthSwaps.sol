5762248eaa70eb342e41e2d5308965be467a7b69
Pavel Ivanov <grammka@gmail.com>
2018-06-07 19:56:27 +0300

    audit 4.2.1 fix

--- contracts/EthToSmthSwaps.sol
+++ contracts/EthToSmthSwaps.sol
@@ -34,6 +34,7 @@ contract EthToSmthSwaps {
   // ETH Owner signs swap
   // initializing time for correct work of close() method
   function sign(address _participantAddress) public {
+    require(swaps[msg.sender][_participantAddress].balance == 0);
     participantSigns[msg.sender][_participantAddress] = now;
   }
 
--- contracts/EthTokenToSmthSwaps.sol
+++ contracts/EthTokenToSmthSwaps.sol
@@ -36,6 +36,7 @@ contract EthTokenToSmthSwaps {
   // ETH Owner signs swap
   // initializing time for correct work of close() method
   function sign(address _participantAddress) public {
+    require(swaps[msg.sender][_participantAddress].balance == 0);
     participantSigns[msg.sender][_participantAddress] = now;
   }
 
