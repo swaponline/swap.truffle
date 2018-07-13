fa6c5083a07c66bcd07dba741f81e9411f754a50
Aleksey Bykhun <theisease@gmail.com>
2018-07-14 00:51:36 +0300

    x fix tests

--- truffle.js
+++ truffle.js
@@ -1,4 +1,17 @@
+let HDWalletProvider = require('truffle-hdwallet-provider')
+let mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+let rinkebyURL = "https://rinkeby.infura.io/sF8QaFr5COSzwukN3V2Y"
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function () { return new HDWalletProvider(mnemonic, rinkebyURL) },
+      network_id: '4',
+    }
+  }
+}
