51e1127f8687a93ac7f75dc3810acc63b772e11d
Aleksey Bykhun <theisease@gmail.com>
2019-01-17 18:09:52 +0300

    truffle deploy to rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-const Reputation            = artifacts.require('./Reputation.sol')
 const EthToSmthSwaps        = artifacts.require('./EthToSmthSwaps.sol')
 const EthTokenToSmthSwaps   = artifacts.require('./EthTokenToSmthSwaps.sol')
 const Token                 = artifacts.require('./Token.sol')
@@ -7,7 +6,6 @@ const Token                 = artifacts.require('./Token.sol')
 module.exports = function (deployer) {
   Promise.all([
     deployer.deploy(Token, 'Test', 'TST', 18),
-    deployer.deploy(Reputation),
     deployer.deploy(EthToSmthSwaps),
     deployer.deploy(EthTokenToSmthSwaps),
   ])
--- truffle.js
+++ truffle.js
@@ -1,6 +1,9 @@
-let HDWalletProvider = require('truffle-hdwallet-provider')
-let mnemonic = "prosper blame pigeon what chest volume whip toast avocado tennis hotel script"
-let rinkebyURL = "https://rinkeby.infura.io/sF8QaFr5COSzwukN3V2Y"
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const mnemonic = process.env.MNEMONIC || "prosper blame pigeon what chest volume whip toast avocado tennis hotel script"
+// let rinkebyURL = "https://rinkeby.infura.io/sF8QaFr5COSzwukN3V2Y"
+// let rinkebyURL = "https://tgeth.swaponline.site"
+const rinkebyURL = "https://rinkeby.infura.io/JCnK5ifEPH9qcQkX0Ahl"
+const mainnetURL = "https://mainnet.infura.io/JCnK5ifEPH9qcQkX0Ahl"
 
 module.exports = {
   networks: {
@@ -13,6 +16,11 @@ module.exports = {
       provider: function () { return new HDWalletProvider(mnemonic, rinkebyURL) },
       gas: 5000000,
       network_id: '4',
+    },
+    mainnet: {
+      provider: function () { return new HDWalletProvider(mnemonic, mainnetURL) },
+      gas: 5000000,
+      network_id: '1',
     }
   },
   mocha: {
